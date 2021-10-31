7bfb96291fd7482acc7e56405648d985aa8d9c52
Abdurrahman BULUT <abdurrahmanbulut@yahoo.com>
2021-10-31 21:32:29 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'splash_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle.dark.copyWith(statusBarColor: Colors.transparent),
+    );
+
+    return MaterialApp(
+      title: 'Introduction screen',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.lightGreen),
+      home: const Splash(),
+    );
+  }
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          appBar: AppBar(),
-        ),
-      ),
+    return const Scaffold(
+      body: SplashScreen(),
     );
   }
 }
+
+
